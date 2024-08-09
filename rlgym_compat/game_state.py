ce2ff3868335696a4cf8f055147f40f1ba4ea533
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2024-08-09 09:44:19 -0400

    v5 changes and some of my improvements

--- rlgym_compat/game_state.py
+++ rlgym_compat/game_state.py
@@ -1,15 +1,14 @@
 import numpy as np
 from typing import List, Optional
 
-from rlbot.utils.structures.game_data_struct import GameTickPacket, FieldInfoPacket, PlayerInfo
-
+from rlbot.flat import *
 from .physics_object import PhysicsObject
 from .player_data import PlayerData
 
 
 class GameState:
-    def __init__(self, game_info: FieldInfoPacket):
-        self.game_type: int = 0 # TODO: perhaps update this according to match settings
+    def __init__(self, game_info: FieldInfo):
+        self.game_type: int = 0  # TODO: perhaps update this according to match settings
         self.blue_score = 0
         self.orange_score = 0
         self.last_touch: Optional[int] = -1
@@ -17,35 +16,36 @@ class GameState:
         self.players: List[PlayerData] = []
         self._on_ground_ticks = np.zeros(64)
         self._air_time_since_jump = np.zeros(64)
+        self.has_jumped = [False] * 10
 
         self.ball: PhysicsObject = PhysicsObject()
         self.inverted_ball: PhysicsObject = PhysicsObject()
 
         # List of "booleans" (1 or 0)
-        self.boost_pads: np.ndarray = np.zeros(game_info.num_boosts, dtype=np.float32)
+        self.boost_pads: np.ndarray = np.zeros(len(game_info.boost_pads), dtype=np.float32)
         self.inverted_boost_pads: np.ndarray = np.zeros_like(self.boost_pads, dtype=np.float32)
 
     def decode(self, packet: GameTickPacket, ticks_elapsed=1, tick_skip=8):
         self.blue_score = packet.teams[0].score
         self.orange_score = packet.teams[1].score
 
-        for i in range(packet.num_boost):
-            self.boost_pads[i] = packet.game_boosts[i].is_active
+        for i, pad in enumerate(packet.boost_pad_states):
+            self.boost_pads[i] = pad.is_active
         self.inverted_boost_pads[:] = self.boost_pads[::-1]
 
-        self.ball.decode_ball_data(packet.game_ball.physics)
+        self.ball.decode_ball_data(packet.balls[0].physics)
         self.inverted_ball.invert(self.ball)
 
         self.players = []
-        latest_touch = packet.game_ball.latest_touch
-        for i in range(packet.num_cars):
-            player = self._decode_player(packet.game_cars[i], i, ticks_elapsed)
-            if latest_touch.time_seconds > 0 and i == latest_touch.player_index and packet.game_info.seconds_elapsed - latest_touch.time_seconds < tick_skip / 120:
+        latest_touch = packet.balls[0].latest_touch
+        for i, car in enumerate(packet.players):
+            player = self._decode_player(car, i, ticks_elapsed)
+            if latest_touch.game_seconds > 0 and i == latest_touch.player_index and packet.game_info.seconds_elapsed - latest_touch.game_seconds < tick_skip / 120:
                 player.ball_touched = True
             
             self.players.append(player)
         
-        if latest_touch.time_seconds > 0:
+        if latest_touch.game_seconds > 0:
             self.last_touch = latest_touch.player_index
         
 
@@ -55,12 +55,22 @@ class GameState:
         player_data.car_data.decode_car_data(player_info.physics)
         player_data.inverted_car_data.invert(player_data.car_data)
 
-        if player_info.has_wheel_contact:
+        if player_info.air_state == AirState.OnGround:
             self._on_ground_ticks[index] = 0
             self._air_time_since_jump[index] = 0
+            self.has_jumped[index] = False
         else:
             self._on_ground_ticks[index] += ticks_elapsed
-            if player_info.jumped: # Technically this should only start when you stop holding jump
+
+            if player_info.air_state == AirState.Jumping:
+                self._air_time_since_jump[index] = 0
+                self.has_jumped[index] = True
+            elif player_info.air_state in {
+                AirState.DoubleJumping,
+                AirState.Dodging,
+            }:
+                self._air_time_since_jump[index] = 150
+            else:
                 self._air_time_since_jump[index] += ticks_elapsed
 
         player_data.car_id = index
@@ -74,11 +84,11 @@ class GameState:
                 player_data.boost_pickups = 1
             else:
                 player_data.boost_pickups += 1
-        player_data.is_demoed = player_info.is_demolished
-        player_data.on_ground = player_info.has_wheel_contact or self._on_ground_ticks[index] <= 6
+        player_data.is_demoed = player_info.demolished_timeout > 0
+        player_data.on_ground = player_info.air_state == AirState.OnGround or self._on_ground_ticks[index] <= 6
         player_data.ball_touched = False
-        player_data.has_jump = not player_info.jumped
-        player_data.has_flip = self._air_time_since_jump[index] < 150 and not player_info.double_jumped
+        player_data.has_jump = not self.has_jumped
+        player_data.has_flip = self._air_time_since_jump[index] < 150
         player_data.boost_amount = player_info.boost / 100
 
         return player_data
