643f916f4961fe5973e22a7e87ff3376975965a8
Jonathan Keegan <jpk3.14@gmail.com>
2025-05-18 20:09:59 -0400

    update version.py

--- rlgym_compat/version.py
+++ rlgym_compat/version.py
@@ -5,9 +5,12 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 release_notes = {
+    "2.0.1": """
+    - Fix can_flip property when on ground to better match RLGym
+    """,
     "2.0.0": """
     - Modify for RLGym v2 and RLBot v5
     """,
