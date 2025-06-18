27a740b8cdb04879503bd81e5b543bf73fe512af
Jonathan Keegan <jpk3.14@gmail.com>
2025-06-17 20:34:52 -0400

    add GameConfig to package import

--- rlgym_compat/__init__.py
+++ rlgym_compat/__init__.py
@@ -1,5 +1,6 @@
 from .car import Car
 from .common_values import BLUE_TEAM, ORANGE_TEAM
+from .game_config import GameConfig
 from .game_state import GameState
 from .physics_object import PhysicsObject
 from .v1 import PhysicsObject as V1PhysicsObject
