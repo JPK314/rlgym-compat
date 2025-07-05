1f8876bb7825478e94a57f811a775f89da22125e
Jonathan Keegan <jpk3.14@gmail.com>
2025-07-04 20:41:41 -0400

    modernize ball touch tracking

--- rlgym_compat/v1_game_state.py
+++ rlgym_compat/v1_game_state.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Optional
 import numpy as np
 from rlbot.flat import FieldInfo, GamePacket, MatchConfiguration, MatchPhase
 
-from .common_values import BLUE_TEAM, ORANGE_TEAM
+from .common_values import BLUE_TEAM, ORANGE_TEAM, TICKS_PER_SECOND
 from .extra_info import ExtraPacketInfo
 from .game_state import GameState
 from .v1.physics_object import PhysicsObject as V1PhysicsObject
@@ -26,13 +26,15 @@ class V1GameState:
         self.blue_score = 0
         self.orange_score = 0
         self.last_touch: Optional[int] = -1
-        self._boost_pickups: Dict[int, int] = {}
         self.players: List[V1PlayerData] = []
         self.ball: V1PhysicsObject = None
         self.inverted_ball: V1PhysicsObject = None
         self.boost_pads: np.ndarray = None
         self.inverted_boost_pads: np.ndarray = None
         self._sort_players_by_car_id = sort_players_by_car_id
+        self._boost_pickups: Dict[int, int] = {}
+        self._car_ball_touched: Dict[int, bool] = {}
+        self._tick_skip = tick_skip
 
     def _recalculate_fields(self):
         spawn_id_spectator_id_map = {}
@@ -47,10 +49,12 @@ class V1GameState:
                 spawn_id_spectator_id_map[spawn_id] = orange_spectator_id
                 orange_spectator_id += 1
         for player_data in self.players:
+            spawn_id = player_data.spawn_id
             player_data.update_from_v2(
-                self._game_state.cars[player_data.spawn_id],
-                spawn_id_spectator_id_map[player_data.spawn_id],
-                self._boost_pickups[player_data.spawn_id],
+                self._game_state.cars[spawn_id],
+                spawn_id_spectator_id_map[spawn_id],
+                self._boost_pickups[spawn_id],
+                self._car_ball_touched[spawn_id],
             )
         if self._sort_players_by_car_id:
             self.players.sort(key=lambda p: p.car_id)
@@ -86,8 +90,22 @@ class V1GameState:
         self._game_state.update(packet, extra_info)
         self.players: List[V1PlayerData] = []
         for player_info in packet.players:
-            if player_info.spawn_id not in self._boost_pickups:
-                self._boost_pickups[player_info.spawn_id] = 0
+            spawn_id = player_info.spawn_id
+            if spawn_id not in self._boost_pickups:
+                self._boost_pickups[spawn_id] = 0
+            # We can't use the RLGym v2's car ball touches since those are tracked per action sequence (with some offset based on delay usage) instead of based on tick skip, so calculate them here
+            if player_info.latest_touch is not None:
+                ticks_since_touch = int(
+                    round(
+                        (
+                            packet.match_info.seconds_elapsed
+                            - player_info.latest_touch.game_seconds
+                        )
+                        * TICKS_PER_SECOND
+                    )
+                )
+                if ticks_since_touch < self._tick_skip:
+                    self._car_ball_touched[spawn_id] = True
             if (
                 packet.match_info.match_phase in (MatchPhase.Active, MatchPhase.Kickoff)
                 and old_boost_amounts[player_info.spawn_id] < player_info.boost / 100
